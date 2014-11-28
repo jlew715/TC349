2605808a614090c7e8e32b6df74b4f0c3386c559
Jerold Lewis <jeroldlewis@Jerolds-MacBook-Air.local>
2014-11-27 22:33:36 -0500

    final work

--- final/finalProj/js/main.js
+++ final/finalProj/js/main.js
@@ -1,1 +1,14 @@
-
+$(document).ready(function() {
+  $(".homeLink").click(function() {
+    window.open("index.html",'_self');
+  });
+  $(".aboutLink").click(function() {
+    window.open("about.html",'_self');
+  });
+  $(".contactLink").click(function() {
+    window.open("contact.html",'_self');
+  });
+  $(".galleryLink").click(function() {
+    window.open("gallery.html",'_self');
+  });
+});
